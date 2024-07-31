e68bb2c3ecc5879197bd7b1e9d1b398b16e62251
rayan ribeiro <rayanribeiro99@gmail.com>
2024-07-31 00:31:43 -0300

    Setando configuração inicial

---
+++ eslint.config.mjs
@@ -0,0 +1,12 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginReact from "eslint-plugin-react";
+
+
+export default [
+  {files: ["**/*.{js,mjs,cjs,jsx}"]},
+  {languageOptions: { globals: globals.browser }},
+  {parser: 'babel-eslint'},
+  pluginJs.configs.recommended,
+  pluginReact.configs.flat.recommended,
+];
